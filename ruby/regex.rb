0e37bbf61c4798875bfa24aefc267e5eb4cd4aac
tmc <chie.tm2002@gmail.com>
2017-11-15 10:15:02 +0700

    update finding header legison

--- ruby/regex.rb
+++ ruby/regex.rb
@@ -3,5 +3,5 @@ module REGEX
   BASEON_FILTER = /\nCăn cứ (.+)/
   CHAPTER_FILTER = /\n(CHƯƠNG|Chương)\s(M{1,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})|M{0,4}(CM|C?D|D?C{1,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})|M{0,4}(CM|CD|D?C{0,3})(XC|X?L|L?X{1,3})(IX|IV|V?I{0,3})|M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|I?V|V?I{1,3}))\s+(.+)/
   PARTICLE_FILTER = /\n.iê?.(u|ù)\s+(\d+)\.\s+(.+)/
-  TEST = "adf"
+
 end
